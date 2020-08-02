e17932c44c772963084f0f613ee1938082361bde
jeriscc <jeriscc@users.noreply.github.com>
2020-08-02 14:23:40 -0700

    refactor functions with express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,110 +1,85 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as _cors from "cors";
+import * as express from "express";
+import * as cors from "cors";
 import { Auditionee, postSchema } from "./types";
 
-const cors = _cors({ origin: true });
+const app = express();
+app.use(cors({ origin: true }));
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 const auditioneesRef = db.collection("auditionees");
 
-export const postAuditionee = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    return cors(req, res, () => {
-      if (req.method !== "POST") {
-        return res.status(401).json({
-          message: "Not allowed"
-        });
-      }
+app.post("/", (req, res) => {
+  const { error, value } = postSchema.validate(req.body);
 
-      const { error, value } = postSchema.validate(req.body);
-
-      if (error !== null) {
-        console.log(error);
-        return res.status(422).json({
-          message: "Invalid input. " + error
-        });
-      }
-
-      return auditioneesRef
-        .add(value)
-        .then(ref => {
-          res.status(200).json({
-            message: "Added document with ID: " + ref.id
-          });
-        })
-        .catch(err => {
-          res.status(err.code).json({
-            message: `Error posting document. ${err.message}`
-          });
-        });
+  if (error !== null) {
+    console.log(error);
+    return res.status(422).json({
+      message: "Invalid input. " + error
     });
   }
-);
 
-export const getAuditionee = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    return cors(req, res, () => {
-      if (req.method !== "GET") {
-        return res.status(401).json({
-          message: "Not allowed"
-        });
-      }
-      const id = req.query.id;
-
-      return auditioneesRef
-        .doc(`${id}`)
-        .get()
-        .then(doc => {
-          if (!doc.exists) {
-            res.status(404).json({
-              message: "No document found"
-            });
-          } else {
-            res.status(200).json(doc.data());
-          }
-        })
-        .catch(err => {
-          res.status(err.code).json({
-            message: `Error getting document. ${err.message}`
-          });
-        });
+  return auditioneesRef
+    .add(value)
+    .then(ref => {
+      res.status(200).json({
+        message: "Added document with ID: " + ref.id
+      });
+    })
+    .catch(err => {
+      res.status(err.code).json({
+        message: `Error posting document. ${err.message}`
+      });
     });
-  }
-);
+});
 
-export const getAuditionees = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    return cors(req, res, () => {
-      if (req.method !== "GET") {
-        return res.status(401).json({
-          message: "Not allowed"
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  return auditioneesRef
+    .doc(`${id}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        res.status(404).json({
+          message: "No document found"
         });
+      } else {
+        res.status(200).json(doc.data());
       }
-
-      const auditionees: Auditionee[] = [];
-
-      return auditioneesRef
-        .get()
-        .then(snapshot => {
-          snapshot.forEach(doc => {
-            auditionees.push({
-              id: doc.id,
-              email: doc.data().email,
-              name: doc.data().name,
-              number: doc.data().number,
-              picture: doc.data().picture,
-              voice_part: doc.data().voice_part
-            });
-          });
-          res.status(200).json(auditionees);
-        })
-        .catch(err => {
-          res.status(err.code).json({
-            message: `Error getting documents. ${err.message}`
-          });
-        });
+    })
+    .catch(err => {
+      res.status(err.code).json({
+        message: `Error getting document. ${err.message}`
+      });
     });
-  }
-);
+})
+
+app.get("/", (req, res) => {
+  const auditionees: Auditionee[] = [];
+
+  return auditioneesRef
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        auditionees.push({
+          id: doc.id,
+          email: doc.data().email,
+          name: doc.data().name,
+          number: doc.data().number,
+          picture: doc.data().picture,
+          voice_part: doc.data().voice_part
+        });
+      });
+      res.status(200).json(auditionees);
+    })
+    .catch(err => {
+      res.status(err.code).json({
+        message: `Error getting documents. ${err.message}`
+      });
+    });
+})
+
+export const auditionee = functions.https.onRequest(app);
